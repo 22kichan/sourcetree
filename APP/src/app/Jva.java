6a4a390c757b63dce2bad70401dd32f07899bf34
KichanLee <72385451+KichanLee@users.noreply.github.com>
2021-03-04 23:22:22 +0900

    변경

---
+++ APP/src/app/Jva.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Jva {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
---
+++ APP/src/app/Study.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Study {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+	}
+
+}
